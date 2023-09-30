b37bf1f4a0086a4f6cf59433e457dbb6c6f47364
premsgar <premsgar@DESKTOP-73K6JOR>
2023-09-30 13:11:46 +0530

    tested test1 method

--- A/src/com/nt/dao/DemoFirstGit.java
+++ A/src/com/nt/dao/DemoFirstGit.java
@@ -4,7 +4,10 @@ public class DemoFirstGit {
 
 	public static void main(String[] args) {
 	
-System.out.println("hello");
+      System.out.println("hello");
+      B b=new B();
+     int x= b.Test1(22, 12);
+     System.out.println(x);
 	}
 
 }
