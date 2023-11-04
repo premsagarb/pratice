1b771a05156e700bc88ce9c479ae966a7246926c
premsgar <premsgar@DESKTOP-73K6JOR>
2023-11-04 10:23:25 +0530

    wrote and tested get method

--- GitPractice11/src/com/Test.java
+++ GitPractice11/src/com/Test.java
@@ -1,7 +1,13 @@
 package com;
 
 public class Test {
+	public void get() {
+		System.out.println("Test.get()");
+	}
 public static void main(String[] args) {
 	System.out.println("welcome to git");
+	Test t=new Test();
+	t.get();
+	
 }
 }
