b890772cfa763b168aa92f021f900d7806266fb2
premsgar <premsgar@DESKTOP-73K6JOR>
2023-11-04 10:38:19 +0530

    g

--- GitPractice11/src/com/Test.java
+++ GitPractice11/src/com/Test.java
@@ -4,7 +4,6 @@ public class Test {
 	
 public static void main(String[] args) {
 	System.out.println("welcome to git");
-	System.out.println("Test.main()");
-	System.out.println("Test.main()");
+	
 }
 }
