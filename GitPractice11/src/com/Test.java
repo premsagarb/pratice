0756fcc7848fb7b85bd20d6197b696d074565e3c
premsgar <premsgar@DESKTOP-73K6JOR>
2023-11-04 11:15:16 +0530

    w

--- GitPractice11/src/com/Test.java
+++ GitPractice11/src/com/Test.java
@@ -3,10 +3,5 @@ package com;
 public class Test {
 	
 public static void main(String[] args) {
-	System.out.println("welcome to git");
-	System.out.println("Test.main()");
-}
-public static void king() {
-	System.out.println("Test.king()");
-}
+	
 }
